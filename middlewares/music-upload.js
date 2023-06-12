0e4c72b51229239b9150d690f8b507bf7607248a
Shreyansh Chachaundiya <shreyansh1647@gmail.com>
2023-06-12 13:50:09 +0530

    cloud setup music

--- middlewares/music-upload.js
+++ middlewares/music-upload.js
@@ -4,12 +4,12 @@ const HttpError = require("../models/http-error");
 const musicUpload = multer({
   limit: 500000,
   storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "./uploads/musics"); // Set the destination folder for storing uploaded videos
-    },
-    filename: (req, file, cb) => {
-      cb(null, Date.now() + "-" + file.originalname); // Set the filename to include the current timestamp
-    },
+    // destination: (req, file, cb) => {
+    //   cb(null, "./uploads/musics"); // Set the destination folder for storing uploaded videos
+    // },
+    // filename: (req, file, cb) => {
+    //   cb(null, Date.now() + "-" + file.originalname); // Set the filename to include the current timestamp
+    // },
   }),
   // fileFilter: (req, file, cb) => {
   //   // Check the file mimetype
